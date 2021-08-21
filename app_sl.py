71f81cb536f60ffea2d9462cb3dd055b5b15f130
Sebastian Guinazu <sebastianguinazu@gmail.com>
2021-08-21 10:20:40 -0300

    correcciones

--- app_sl.py
+++ app_sl.py
@@ -19,23 +19,23 @@ def main():
     st.sidebar.header('Elija el monitor')
 
     page = st.sidebar.selectbox('Distribucion de los puntajes', ["Homepage", "General", "Tipo de socio", \
-        "Grupo etario", "Genero", "Socio cancha", "Descargar csv"])
+        "Grupo etario", "Genero", "Socio cancha", "Analisis puntual", "Descargar csv"])
 
     st.sidebar.header('Elija puntajes')
     patrimonial = st.sidebar.slider("Patrimonial pts:", 0, 2000, 2000, 100, '%d')
     refundador = st.sidebar.slider("Refundador pts", 0, 2000, 1500, 100, '%d')
     debito = st.sidebar.slider("Debito pts", 0, 2000, 1000, 100, '%d')
     cuota_dia_aspo = st.sidebar.slider("Cuota al dia ASPO pts", 0, 2000, 1000, 100, '%d')
-    ant_simple = st.sidebar.slider("Antiguedad Simple pts", 0, 1000, 250, 50, '%d')
-    ant_pleno = st.sidebar.slider("Antiguedad Pleno pts", 0, 1000, 350, 50, '%d')
-    ant_interior = st.sidebar.slider("Antiguedad Interior pts", 0, 1000, 350, 50, '%d')
-    ant_exterior = st.sidebar.slider("Antiguedad Exterior pts", 0, 1000, 500, 50, '%d')
-    ant_vitalic = st.sidebar.slider("Antiguedad Vitalicio pts", 0, 1000, 350, 50, '%d')
+    ant_simple = st.sidebar.slider("Antiguedad Simple pts", 0, 2000, 250, 50, '%d')
+    ant_pleno = st.sidebar.slider("Antiguedad Pleno pts", 0, 2000, 350, 50, '%d')
+    ant_interior = st.sidebar.slider("Antiguedad Interior pts", 0, 2000, 350, 50, '%d')
+    ant_exterior = st.sidebar.slider("Antiguedad Exterior pts", 0, 2000, 500, 50, '%d')
+    ant_vitalic = st.sidebar.slider("Antiguedad Vitalicio pts", 0, 2000, 350, 50, '%d')
     abo_bidegain = st.sidebar.slider("Abono Bidegain pts", 0, 2000, 1000, 100, '%d')
     abo_polideportivo = st.sidebar.slider("Abono Polideportivo pts", 0, 2000, 500, 100, '%d')
     # vita_abonando = st.sidebar.slider("Vitalicio cuota pts", 0, 2000, 25, 100, '%d')
-    eve_bid_amba = st.sidebar.slider("Bidegain Ingreso AMBA pts", 0, 500, 50, 25, '%d')
-    eve_bid_inte = st.sidebar.slider("Bidegain Ingreso Interior pts", 0, 500, 75, 25, '%d')
+    eve_bid_amba = st.sidebar.slider("Bidegain Ingreso AMBA pts", 0, 2000, 50, 25, '%d')
+    eve_bid_inte = st.sidebar.slider("Bidegain Ingreso Interior pts", 0, 2000, 75, 25, '%d')
     # eve_pol_amba = st.sidebar.slider("Polideportivo Ingreso AMBA pts", 0, 500, 50, 25, '%d')
     # eve_pol_inte = st.sidebar.slider("Polideportivo Ingreso Interior pts", 0, 500, 75, 25, '%d')
     mult_mujer = st.sidebar.selectbox('Multiplicador Mujeres', [1,1.5,2])
@@ -50,7 +50,7 @@ def main():
     if page == "Homepage":
     	st.image('helpers/sl_cabecera.PNG')
     	st.title("Simulador de puntos de Fidelidad")
-    	st.markdown("> Por Sebastian Guinazu (Socio Nro 29873)")
+    	# st.markdown("> Por Sebastian Guinazu (Socio Nro 29873)")
     	st.markdown("Esta app se construyo con el objetivo de poder simular el puntaje de fidelidad de \
             los socios San Lorenzo. En la barra de la izquierda se pueden elegir los puntajes de los \
             distintos items. Tambien se pueden seleccionar diferentes monitores para analizar la distribucion\
@@ -122,9 +122,6 @@ def main():
         st.write(f"- Los socios **entre 18 y 29** son el {round(df[df['grupo_etario']=='Entre 18 y 29'].shape[0]/df.shape[0]*100)}%. \
             Su promedio de pts de fidelidad queda en {round(df.loc[df['grupo_etario']=='Entre 18 y 29', 'puntos'].mean())}\
             y la mediana es {round(df.loc[df['grupo_etario']=='Entre 18 y 29', 'puntos'].describe()[5])}")
-        st.write(f"- Los socios **menores a 18** son el {round(df[df['grupo_etario']=='Menores a 18'].shape[0]/df.shape[0]*100)}%. \
-            Su promedio de pts de fidelidad queda en {round(df.loc[df['grupo_etario']=='Menores a 18', 'puntos'].mean())}\
-            y la mediana es {round(df.loc[df['grupo_etario']=='Menores a 18', 'puntos'].describe()[5])}")
 
         st.markdown("### Grafico de la distribucion")
         if st.button('Mostrar'):
@@ -175,13 +172,30 @@ def main():
                 category_orders={"partidos": ["Ninguno", "Entre 1 y 4", "Entre 5 y 9", "Mas de 10"]})
             st.plotly_chart(fig, use_container_width=True)
 
+    elif page == "Analisis puntual":
+        st.title("Analisis puntual por Nro de Socio")
+        st.markdown("")
+        st.markdown("Este monitor permite analizar las variables que forman parte del plan de fidalidad para algun \
+            socio en particular.")
+
+        mostrar = st.button('Mostrar info')
+        numero_socio = st.number_input('Numero de Socio', min_value=0, max_value=999999, value=0)
+        
+        if mostrar:
+            df_socio = df.copy()
+            df_socio = df_socio[df_socio.index == str(numero_socio)]
+            st.write(df_socio)
+
+
     elif page == "Descargar csv":
         st.title("Descargar csv filtrado")
         st.markdown("")
         st.markdown("En este monitor se pueden fijar ciertos filtros para descargar el csv con la info de los socios. \
             Una vez que estan definidos, tocar le boton 'aplicar filtros', luego, ir abajo y tocar 'Descargar csv file'.")
-        filtros=st.button('Aplicar filtros')
+        filtros = st.button('Aplicar filtros')
 
+        filtro_puntos_min = st.number_input('Puntos min', min_value=0, max_value=df.puntos.max(), value=0)
+        filtro_puntos_max = st.number_input('Puntos max', min_value=0, max_value=df.puntos.max(), value=df.puntos.max())
         filtro_edad_min = st.number_input('Edad min', min_value=0, max_value=100, value=18)
         filtro_edad_max = st.number_input('Edad max', min_value=0, max_value=100, value=70)
         filtro_tiposocio = st.selectbox('Tipo de socio', (['TODOS'] + list(df['tipo_socio'].unique())))
@@ -196,7 +210,9 @@ def main():
         if filtros:
             'Se aplicaron los filtros'
             import base64
-            df_download = df
+            df_download = df.copy()
+            df_download = df_download[df_download.puntos >= filtro_puntos_min]
+            df_download = df_download[df_download.puntos <= filtro_puntos_max]
             df_download = df_download[df_download.edad >= filtro_edad_min]
             df_download = df_download[df_download.edad <= filtro_edad_max]
             if filtro_tiposocio != 'TODOS':
@@ -210,7 +226,7 @@ def main():
             df_download = df_download[df_download.antig >= filtro_antig_min]
             df_download = df_download[df_download.antig <= filtro_antig_max]
 
-            csv = df_download.to_csv(index=False)
+            csv = df_download.to_csv()
             b64 = base64.b64encode(csv.encode()).decode()  # some strings
             linko= f'<a href="data:file/csv;base64,{b64}" download="myfilename.csv">Download csv file</a>'
             st.markdown(linko, unsafe_allow_html=True)
@@ -220,7 +236,7 @@ def main():
 @st.cache(show_spinner=False, allow_output_mutation=True)
 def load_datasets():
     df = pickle.load( open( "data/df_fide.p", "rb" ) )
-
+    df = df.loc[df['edad']>=18]
     df['puntos'] = 0
     return df
 
@@ -239,6 +255,7 @@ def calc_puntos(df, patrimonial, refundador, debito, cuota_dia_aspo, ant_simple,
     df.loc[df['tipo_socio']=='INTERIOR', 'puntos'] += df.loc[df['tipo_socio']=='INTERIOR', 'antig'] * ant_interior
     df.loc[df['tipo_socio']=='EXTERIOR', 'puntos'] += df.loc[df['tipo_socio']=='EXTERIOR', 'antig'] * ant_exterior
     df.loc[df['tipo_socio']=='VITALICIO', 'puntos'] += df.loc[df['tipo_socio']=='VITALICIO', 'antig'] * ant_vitalic
+    df.loc[df['tipo_socio']=='PARTRIMONIAL', 'puntos'] += df.loc[df['tipo_socio']=='PARTRIMONIAL', 'antig'] * ant_pleno
 
     df['puntos'] += df['abo_bid_2020'] * abo_bidegain
     df['puntos'] += df['abo_bid_2021'] * abo_bidegain
@@ -261,6 +278,10 @@ def calc_puntos(df, patrimonial, refundador, debito, cuota_dia_aspo, ant_simple,
     df.loc[((intext) & (df['sexo']=='F')), 'puntos'] += (df.loc[intext, 'eve_2018'] * eve_bid_inte * cancha_mujer)
     df.loc[((intext) & (df['sexo']=='F')), 'puntos'] += (df.loc[intext, 'eve_2019'] * eve_bid_inte * cancha_mujer)
     df.loc[((intext) & (df['sexo']=='F')), 'puntos'] += (df.loc[intext, 'eve_2020'] * eve_bid_inte * cancha_mujer)
+
+    df['puntos'] = np.where(df['puntos']<0, 0, df['puntos'])
+
+    df['puntos'] = pd.to_numeric(df['puntos'], downcast='integer')
 
     return df
 
